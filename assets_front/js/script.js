c6ffb9480d6295f3b971ab3b4656098fbdb4d46d
nazaporta <nazarena@porta.com.py>
2025-04-13 21:31:47 -0300

    add menu mobile

--- assets_front/js/script.js
+++ assets_front/js/script.js
@@ -1,6 +1,22 @@
+/* menu mobile */
+const menuToggle = document.querySelector("#menuToggle");
+const openMenu = document.querySelector("#openMenu");
+const closeMenu = document.querySelector("#closeMenu");
+
+openMenu.addEventListener("click", () => {
+  menuToggle.classList.add("visible");
+});
+
+closeMenu.addEventListener("click", () => {
+  menuToggle.classList.remove("visible");
+});
+/* menu mobile */
+
+/* scroll button */
 function bounce(element) {
   element.style.transform = "scale(0.8)";
   setTimeout(function () {
     element.style.transform = "scale(1)";
   }, 300);
 }
+/* scroll button */
